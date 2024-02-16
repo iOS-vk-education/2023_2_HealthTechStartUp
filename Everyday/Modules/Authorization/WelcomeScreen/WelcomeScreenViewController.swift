6e96cca5fdc581dd5cbadd9be1333d30ce6f0345
MX126 <mikekhristokyan@gmail.com>
2024-02-16 16:16:35 +0300

    edit sceneDelegate

---
+++ Everyday/Modules/Authorization/WelcomeScreen/WelcomeScreenViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WelcomeScreenViewController.swift
+//  Everyday
+//
+//  Created by Михаил on 16.02.2024.
+//
+
+import UIKit
+
+final class WelcomeScreenViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
--- Everyday/Support/SceneDelegate.swift
+++ Everyday/Support/SceneDelegate.swift
@@ -12,23 +12,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let x = (scene as? UIWindowScene) else {
+        guard let scene = (scene as? UIWindowScene) else {
             return
         }
-    }
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-    }
+        // let viewController = WelcomeScreenContainer.assemble(with: .init()).viewController
+        let viewController = WelcomeScreenViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
 
-    func sceneDidBecomeActive(_ scene: UIScene) {
-    }
+        let window = UIWindow(windowScene: scene)
+        self.window = window
 
-    func sceneWillResignActive(_ scene: UIScene) {
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
     }
 }
