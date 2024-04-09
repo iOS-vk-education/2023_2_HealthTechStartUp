f8fd666d25f553647bb2e0c5534ca90fd96524bc
Alexander <fix1in1it@gmail.com>
2024-04-09 17:17:39 +0300

    settings background color

--- Everyday/Modules/Settings/Settings/SettingsViewController.swift
+++ Everyday/Modules/Settings/Settings/SettingsViewController.swift
@@ -22,6 +22,12 @@ final class SettingsViewController: UIViewController {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemMint
+    }
 }
 
 extension SettingsViewController: SettingsViewInput {
