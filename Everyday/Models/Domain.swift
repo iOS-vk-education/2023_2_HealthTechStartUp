daa68587087238ed747cc0ffcb16efdd3a98e934
Alexander <fix1in1it@gmail.com>
2024-04-24 20:42:13 +0300

    new domain model

--- Everyday/Models/Domain.swift
+++ Everyday/Models/Domain.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+// MARK: - NotepadModel
+
+struct NotepadModel {
+    var schedule: [NewWorkout]?
+    let history: [NewWorkout]?
+}
+
+struct NewWorkout {  // Workout
+    let workoutName: String
+    let dayName: String
+    let dayIndex: Int
+    var sets: [NewTrainingSet]
+}
+
+struct NewTrainingSet {  // TrainingSet
+    let name: String
+    var exercises: [NewExercise]
+}
+
+struct NewExercise {  // Exercise
+    let name: String
+    let description: String
+    var result: String
+}
+
+// MARK: - ExtraModel
+
+struct ExtraModel {  // ExtraModel
+    let imageURL: String?
+    let condition: Int?
+    let weight: Double?
+}
